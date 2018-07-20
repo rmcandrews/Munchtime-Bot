d2f40716ae657f1219c140ee93eb10e7356e62e5
rmcandrews <rmcandre915@gmail.com>
2018-07-19 23:21:28 -0500

    Vote to kick (#12)

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ slackEvents.on('message', (event) => {
                         const offenseNumber = userOffenses.length + 1;
                         let offenseTime = getOffenseTime(offenseNumber);
                         if(doesMentionBot(event.text)) {
-                            web.chat.postMessage({ channel: event.channel, text: `${user.real_name} insulted my mother and is therefore kicked for 24 hours.` })
+                            web.chat.postMessage({ channel: event.channel, text: `${user.profile.display_name || user.real_name} insulted my mother and is therefore kicked for 24 hours.` })
                             .then(() => {
                                 const twentyFourHoursInSeconds = 86400;
                                 inviteUserAfterTime(event.channel, event.user, twentyFourHoursInSeconds);
@@ -180,7 +180,7 @@ slackEvents.on('message', (event) => {
                             })
                             .catch(console.error);
                         } else {
-                            web.chat.postMessage({ channel: event.channel, text: `${user.real_name} was kicked for using a banned phrase. This is their ${helpers.ordinalOf(offenseNumber)} offense in the last 24 hours. They will be reinvited after ${offenseTime.words}.` })
+                            web.chat.postMessage({ channel: event.channel, text: `${user.profile.display_name || user.real_name} was kicked for using a banned phrase. This is their ${helpers.ordinalOf(offenseNumber)} offense in the last 24 hours. They will be reinvited after ${offenseTime.words}.` })
                             .then(() => {
                                 inviteUserAfterTime(event.channel, event.user, offenseTime.seconds);
                                 offenseService.createOffense(event.user, offenseTime.seconds);
@@ -251,7 +251,7 @@ slackEvents.on('member_joined_channel', (event) => {
                     .then(userOffenses => {
                         const offenseNumber = userOffenses.length + 1;
                         let offenseTime = getOffenseTime(offenseNumber);
-                        web.chat.postMessage({ channel: event.channel, text: `${user.real_name} was kicked for kicking me. This is their ${helpers.ordinalOf(offenseNumber)} offense in the last 24 hours. They will be reinvited after ${offenseTime.words}.` })
+                        web.chat.postMessage({ channel: event.channel, text: `${user.profile.display_name || user.real_name} was kicked for kicking me. This is their ${helpers.ordinalOf(offenseNumber)} offense in the last 24 hours. They will be reinvited after ${offenseTime.words}.` })
                         .then(() => {
                             inviteUserAfterTime(event.channel, userWhoKickedMe, offenseTime.seconds);
                             offenseService.createOffense(userWhoKickedMe, offenseTime.seconds);
@@ -263,6 +263,81 @@ slackEvents.on('member_joined_channel', (event) => {
             }).catch(console.error);
         }
     }
+});
+
+let voteKickedMessages = [];
+
+slackEvents.on('reaction_added', (event) => {
+    if (event.reaction !== "kick") {
+        return;
+    }
+    
+    let channelId = event.item.channel;
+    let reactionGetOptions = {
+      channel: channelId,
+      timestamp: event.item.ts,
+      file: event.item.file,
+      file_comment: event.item.file_comment,
+      full: true
+    };
+    
+    web.reactions.get(reactionGetOptions).then(response => {
+        let reactedToItem = response[response.type];
+        let reactions = reactedToItem.reactions;
+        let kickReaction = reactions.find(reaction => reaction.name === "kick");
+
+        let hasBeenKickedForMessageAlready = voteKickedMessages.includes(event.item.ts) || voteKickedMessages.includes(event.item.file) || voteKickedMessages.includes(event.item.file_comment);
+        if(kickReaction && kickReaction.count >= 3 && !hasBeenKickedForMessageAlready) {
+            voteKickedMessages.push(event.item.ts || event.item.file || event.item.file_comment);
+            let userId = reactedToItem.user;
+            if(!userId) {
+                userId = event.user
+                web.groups.kick({
+                    channel: channelId,
+                    user: userId
+                }).then(() => {
+                    web.users.info({
+                        user: userId
+                    }).then(response => {
+                        let user = response.user;
+                        offenseService.getOffensesForUserInLast24Hours(userId)
+                        .then(userOffenses => {
+                            const offenseNumber = userOffenses.length + 1;
+                            let offenseTime = getOffenseTime(offenseNumber);
+                            web.chat.postMessage({ channel: channelId, text: `${user.profile.display_name || user.real_name} voted to kick me, so I kicked them. This is their ${helpers.ordinalOf(offenseNumber)} offense in the last 24 hours. They will be reinvited after ${offenseTime.words}.` })
+                            .then(() => {
+                                inviteUserAfterTime(channelId, userId, offenseTime.seconds);
+                                offenseService.createOffense(userId, offenseTime.seconds);
+                                scoresService.updateBanScore(userId, offenseTime.seconds);
+                            }).catch(console.error);
+                        }).catch(console.error);
+                    }).catch(console.error);
+                }).catch(console.error);
+            } else {
+                web.groups.kick({
+                    channel: channelId,
+                    user: userId
+                }).then(() => {
+                    web.users.info({
+                        user: userId
+                    }).then(response => {
+                        let user = response.user;
+                        offenseService.getOffensesForUserInLast24Hours(userId)
+                        .then(userOffenses => {
+                            const offenseNumber = userOffenses.length + 1;
+                            let offenseTime = getOffenseTime(offenseNumber);
+                            web.chat.postMessage({ channel: channelId, text: `The people have spoken. ${user.profile.display_name || user.real_name} has been removed from the chat. This is their ${helpers.ordinalOf(offenseNumber)} offense in the last 24 hours. They will be reinvited after ${offenseTime.words}.` })
+                            .then(() => {
+                                inviteUserAfterTime(channelId, userId, offenseTime.seconds);
+                                offenseService.createOffense(userId, offenseTime.seconds);
+                                scoresService.updateBanScore(userId, offenseTime.seconds);
+                            }).catch(console.error);
+                        }).catch(console.error);
+                    }).catch(console.error);
+                }).catch(console.error);
+            }
+        }
+    }).catch(console.error);
 });
 
 // Handle errors (see `errorCodes` export)
